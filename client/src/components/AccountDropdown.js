9a38bed8639cb2236cb736797d915956602f5e00
Dylan Murphy <dylan.murphy7799@gmail.com>
2025-03-06 01:15:34 +0000

    Frontend continued

--- client/src/components/AccountDropdown.js
+++ client/src/components/AccountDropdown.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
+import "../css/login.css"
 
 export default class AccountDropdown extends Component {
     render() {
@@ -8,26 +9,22 @@ export default class AccountDropdown extends Component {
                 <h2 className="login-title">Login</h2>
                 <form>
                     <div className="login-form">
-                        <label>Username</label>
-                        <input type="text" className="input-field" />
+                        <input type="text" placeholder="email" className="input-field"/>
                     </div>
                     <div className="login-form">
-                        <label>Password</label>
-                        <input type="password" className="input-field" />
+                        <input type="password" placeholder="password" className="input-field"/>
                     </div>
-
 
                     <div className="forgot-password">
                         <Link to="/forgot-password">Forgot Password?</Link>
                     </div>
 
-
-                    <button type="submit" className="login-submit">Login</button>
+                    <button type="submit" className="submit-button">Login</button>
 
 
                     <div className="register-container">
                         <p>Don't have an account?</p>
-                        <Link to="/register" className="register-button">Register</Link>
+                        <button type="submit" className="submit-button">Register</button>
                     </div>
                 </form>
             </div>
