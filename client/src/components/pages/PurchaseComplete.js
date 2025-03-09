b67f20e1f505f95bda3cfb2897eb4ca7ef041fea
Dylan Murphy <dylan.murphy7799@gmail.com>
2025-03-09 18:21:28 +0000

    minor

---
+++ client/src/components/pages/PurchaseComplete.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+import "../../css/purchasecomplete.css"
+
+export default class PurchaseComplete extends Component {
+    render() {
+        return (
+            <div>
+                <h1>purchase complete</h1>
+
+            </div>
+        )
+    }
+}
