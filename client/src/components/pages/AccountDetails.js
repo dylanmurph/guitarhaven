b7aaa41eef78b6574c41e0cab8b8437143be75e9
Dylan Murphy <dylan.murphy7799@gmail.com>
2025-03-09 17:52:08 +0000

    Account details refined

--- client/src/components/pages/AccountDetails.js
+++ client/src/components/pages/AccountDetails.js
@@ -11,8 +11,6 @@ export default class AccountDetails extends Component {
             firstName: "",
             lastName: "",
             email: "",
-            password: "",
-            confirmPassword: "",
             address1: "",
             address2: "",
             county: "",
@@ -21,40 +19,48 @@ export default class AccountDetails extends Component {
             firstNameError: "",
             lastNameError: "",
             emailError: "",
-            passwordError: "",
-            confirmPasswordError: "",
             phoneError: "",
-            isUpdated: false,
-            redirectToHome: false
+            userId: "",
+            isUpdated: false
         }
     }
 
     componentDidMount() {
-        axios.get(`${SERVER_HOST}/users/${this.props.match.params.id}`, { headers: { "authorization": localStorage.token } })
+        const token = localStorage.getItem("token")
+        if (!token || token === "null") {
+            alert("You must be logged in.")
+            return
+        }
+
+        const cfg = {
+            headers: {Authorization: `JWT ${token}`}
+        }
+
+        axios.get(`${SERVER_HOST}/users/`, cfg)
             .then(res => {
-                if (res.data) {
-                    if (res.data.errorMessage) {
-                        console.log(res.data.errorMessage)
-                    } else {
-                        this.setState({
-                            firstName: res.data.firstName,
-                            lastName: res.data.lastName,
-                            email: res.data.email,
-                            address1: res.data.address1,
-                            address2: res.data.address2,
-                            county: res.data.county,
-                            phone: res.data.phone,
-                            image: res.data.image
-                        })
-                    }
+                if (res.data && !res.data.errorMessage) {
+                    this.setState({
+                        userId: res.data.userId,
+                        firstName: res.data.firstName,
+                        lastName: res.data.lastName,
+                        email: res.data.email,
+                        address1: res.data.address1,
+                        address2: res.data.address2,
+                        county: res.data.county,
+                        phone: res.data.phone,
+                        image: res.data.image
+                    })
                 } else {
-                    console.log("Record not found")
+                    console.log("Error: ", res.data.errorMessage || "Record not found")
                 }
+            })
+            .catch(error => {
+                console.log("Error fetching user data:", error)
             })
     }
 
     handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value, [`${e.target.name}Error`]: "" })
+        this.setState({[e.target.name]: e.target.value, [`${e.target.name}Error`]: ""})
     }
 
     handleSubmit = (e) => {
@@ -67,32 +73,22 @@ export default class AccountDetails extends Component {
         let error = false
 
         if (!nameRegex.test(this.state.firstName)) {
-            this.setState({ firstNameError: "Please enter only your first name" })
+            this.setState({firstNameError: "Please enter only your first name"})
             error = true
         }
 
         if (!nameRegex.test(this.state.lastName)) {
-            this.setState({ lastNameError: "Please enter only your last name" })
+            this.setState({lastNameError: "Please enter only your last name"})
             error = true
         }
 
         if (!emailRegex.test(this.state.email)) {
-            this.setState({ emailError: "Please enter a valid email" })
-            error = true
-        }
-
-        if (this.state.password.length < 8) {
-            this.setState({ passwordError: "Password must be at least 8 characters long" })
-            error = true
-        }
-
-        if (this.state.password !== this.state.confirmPassword) {
-            this.setState({ confirmPasswordError: "Passwords do not match" })
+            this.setState({emailError: "Please enter a valid email"})
             error = true
         }
 
         if (!phoneRegex.test(this.state.phone)) {
-            this.setState({ phoneError: "Please enter a valid phone number - format: 871231234" })
+            this.setState({phoneError: "Please enter a valid phone number - format: 871231234"})
             error = true
         }
 
@@ -113,17 +109,27 @@ export default class AccountDetails extends Component {
             updatedUser.password = this.state.password
         }
 
-        axios.put(`${SERVER_HOST}/users/${this.props.match.params.id}`, updatedUser, { headers: { "authorization": localStorage.token } })
+        const token = localStorage.getItem("token")
+        if (!token || token === "null") {
+            alert("You must be logged in to update your account.")
+            return
+        }
+
+        const cfg = {
+            headers: {Authorization: `JWT ${token}`}
+        }
+
+        axios.put(`${SERVER_HOST}/users/${this.state.userId}`, updatedUser, cfg)
             .then(res => {
                 if (res.data && res.data.errorMessage) {
                     console.log(res.data.errorMessage)
                 } else {
                     console.log("User Updated successfully")
-                    this.setState({ isUpdated: true, redirectToHome: true })
+                    this.setState({isUpdated: true})
                 }
             })
             .catch(error => {
-                console.log("Error during Updating:", error)
+                console.log("Error during updating:", error)
             })
     }
 
@@ -138,43 +144,33 @@ export default class AccountDetails extends Component {
 
         return (
             <div className="form-container">
-                {this.state.redirectToHome ? <Redirect to="/home" /> : null}
+                {this.state.isUpdated ? <Redirect to="/home"/> : null}
 
                 <form className="form" onSubmit={this.handleSubmit}>
                     <h2>Modify Account</h2>
 
                     <div>
                         <input className="input-field" type="text" name="firstName" placeholder="First Name"
-                               value={this.state.firstName} onChange={this.handleChange} required />
+                               value={this.state.firstName} onChange={this.handleChange} required/>
                         <p className="error-message">{this.state.firstNameError}</p>
                     </div>
                     <div>
                         <input className="input-field" type="text" name="lastName" placeholder="Last Name"
-                               value={this.state.lastName} onChange={this.handleChange} required />
+                               value={this.state.lastName} onChange={this.handleChange} required/>
                         <p className="error-message">{this.state.lastNameError}</p>
                     </div>
                     <div>
                         <input className="input-field" type="text" name="email" placeholder="Email"
-                               value={this.state.email} onChange={this.handleChange} required />
+                               value={this.state.email} onChange={this.handleChange} required/>
                         <p className="error-message">{this.state.emailError}</p>
                     </div>
                     <div>
-                        <input className="input-field" type="password" name="password" placeholder="Password"
-                               value={this.state.password} onChange={this.handleChange} />
-                        <p className="error-message">{this.state.passwordError}</p>
-                    </div>
-                    <div>
-                        <input className="input-field" type="password" name="confirmPassword" placeholder="Confirm Password"
-                               value={this.state.confirmPassword} onChange={this.handleChange} />
-                        <p className="error-message">{this.state.confirmPasswordError}</p>
-                    </div>
-                    <div>
                         <input className="input-field" type="text" name="address1" placeholder="Address Line 1"
-                               value={this.state.address1} onChange={this.handleChange} required />
+                               value={this.state.address1} onChange={this.handleChange} required/>
                     </div>
                     <div>
                         <input className="input-field" type="text" name="address2" placeholder="Address Line 2"
-                               value={this.state.address2} onChange={this.handleChange} required />
+                               value={this.state.address2} onChange={this.handleChange} required/>
                     </div>
                     <div>
                         <select name="county" value={this.state.county} onChange={this.handleChange}>
@@ -186,12 +182,12 @@ export default class AccountDetails extends Component {
                     </div>
                     <div>
                         <input className="input-field" type="number" name="phone" placeholder="Phone Number"
-                               value={this.state.phone} onChange={this.handleChange} required />
+                               value={this.state.phone} onChange={this.handleChange} required/>
                         <p className="error-message">{this.state.phoneError}</p>
                     </div>
                     <div>
                         <input className="input-field" type="text" name="image" placeholder="Image"
-                               value={this.state.image} onChange={this.handleChange} />
+                               value={this.state.image} onChange={this.handleChange}/>
                     </div>
 
                     <button type="submit">Update</button>
